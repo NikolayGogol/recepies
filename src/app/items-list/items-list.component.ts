75aba96fa573d6eac5dfad0da5a12620b453c400
nikolay <k.gogol13@gmail.com>
2019-12-25 20:17:43 +0200

    remove extra symbols

--- src/app/items-list/items-list.component.ts
+++ src/app/items-list/items-list.component.ts
@@ -3,25 +3,26 @@ import {ItemsService} from '../../services/items.service';
 import {PageEvent} from '@angular/material';
 
 @Component({
-  selector: 'app-list-items',
-  templateUrl: './items-list.component.html',
-  styleUrls: ['./items-list..component.scss']
+    selector: 'app-list-items',
+    templateUrl: './items-list.component.html',
+    styleUrls: ['./items-list..component.scss']
 })
 export class ListItemsComponent implements OnInit {
-  items = [];
-  itemsLenght = 0;
+    items = [];
+    itemsLenght = 0;
 
-  pageEvent: PageEvent;
+    pageEvent: PageEvent;
 
-  constructor(private itemsData: ItemsService) {
-  }
+    constructor(private itemsData: ItemsService) {
 
-  ngOnInit() {
-    this.items = this.itemsData.getAllData();
-    this.itemsLenght = this.items.length
-  }
+    }
 
-  pagination(event) {
-    this.items = this.itemsData.pagination(this.pageEvent);
-  }
+    ngOnInit() {
+        this.items = this.itemsData.getAllData();
+        this.itemsLenght = this.items.length;
+    }
+
+    pagination() {
+        this.items = this.itemsData.pagination(this.pageEvent);
+    }
 }
